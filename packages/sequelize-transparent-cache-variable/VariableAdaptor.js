a5e0e44cb9c9bb6ac4242d1bd73e5163443f83f1
Daniel Hreben <daniel.hreben@gmail.com>
2017-05-22 13:14:27 +0300

    [adaptor-variable] v1.0.0

---
+++ packages/sequelize-transparent-cache-variable/VariableAdaptor.js
@@ -0,0 +1,33 @@
+class VariableAdaptor {
+  constructor(store = {}) {
+    this.store = store
+  }
+
+  _ensureModel(model) {
+    if (!this.store[model]) {
+      this.store[model] = {}
+    }
+  }
+
+  set([model, id], value) {
+    this._ensureModel(model)
+
+    this.store[model][id] = value
+    return Promise.resolve()
+  }
+
+  get([model, id]) {
+    this._ensureModel(model)
+
+    return Promise.resolve(this.store[model][id])
+  }
+
+  del([model, id]) {
+    this._ensureModel(model)
+
+    delete this.store[model][id]
+    Promise.resolve()
+  }
+}
+
+module.exports = VariableAdaptor
