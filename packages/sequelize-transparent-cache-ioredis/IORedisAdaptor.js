4229747aefa01a67b1c344bb1b3ceb05945b9d9c
Daniel Hreben <daniel.hreben@gmail.com>
2017-06-09 21:45:32 +0300

    [adaptor-ioredis] v1.0.0

---
+++ packages/sequelize-transparent-cache-ioredis/IORedisAdaptor.js
@@ -0,0 +1,45 @@
+class IORedisAdaptor {
+  constructor ({ client, namespace, lifetime }) {
+    this.client = client
+    this.namespace = namespace
+    this.lifetime = lifetime
+  }
+
+  _withNamespace (key) {
+    const namespace = this.namespace
+    const keyWithNamespace = namespace
+      ? [namespace, ...key]
+      : key
+
+    return keyWithNamespace.join(':')
+  }
+
+  set (key, value) {
+    const options = this.lifetime
+      ? [ 'EX', this.lifetime ]
+      : []
+
+    return this.client.set(
+      this._withNamespace(key),
+      JSON.stringify(value),
+      options
+    )
+  }
+
+  get (key) {
+    return this.client.get(this._withNamespace(key))
+    .then(data => {
+      if (!data) {
+        return data
+      }
+
+      return JSON.parse(data)
+    })
+  }
+
+  del (key) {
+    return this.client.del(this._withNamespace(key))
+  }
+}
+
+module.exports = IORedisAdaptor
