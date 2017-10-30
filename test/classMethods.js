4a4794d487c78ff8816349c587b59f033b2e9727
Adnene KHALFA <adnene.khalfa@gmail.com>
2017-10-30 20:49:05 +0100

    Closes DanielHreben/sequelize-transparent-cache#2

--- test/classMethods.js
+++ test/classMethods.js
@@ -2,6 +2,7 @@ const t = require('tap')
 const sequelize = require('./helpers/sequelize')
 
 const User = sequelize.models.User
+const Person = sequelize.models.Person
 const cacheStore = User.cache().client().store
 
 t.test('Instance methods', async t => {
@@ -14,17 +15,46 @@ t.test('Instance methods', async t => {
     name: 'Daniel'
   })
 
+  const mike = await Person.cache().create({
+    name: 'Mike'
+  })
+
+  const alex = await Person.cache().create({
+    name: 'Alex'
+  })
+
   t.test('Create', async t => {
     t.deepEqual(
       cacheStore.User[1],
       user.get(),
-      'User cached afrer create'
+      'User with primary key cached after create'
     )
-
+    console.log(cacheStore)
+    t.deepEqual(
+      cacheStore.Person[1],
+      mike.get(),
+      'Entity without primary key cached after create using autoincrement id'
+    )
+    t.deepEqual(
+      cacheStore.Person[2],
+      alex.get(),
+      'Entity without primary key cached after create using autoincrement id'
+    )
     t.deepEqual(
       (await User.cache().findById(1)).get(),
       user.get(),
-      'Cached user correctly loaded'
+      'Cached user with primary key correctly loaded'
+    )
+
+    t.deepEqual(
+      (await Person.cache().findById(1)).get(),
+      mike.get(),
+      'Cached entity without primary key correctly loaded using auto increment id'
+    )
+    t.deepEqual(
+      (await Person.cache().findById(2)).get(),
+      alex.get(),
+      'Cached entity without primary key correctly loaded using auto increment id'
     )
   })
 
