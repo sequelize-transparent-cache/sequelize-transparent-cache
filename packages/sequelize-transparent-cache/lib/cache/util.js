222d056d9423aaf0bb41c7292fd46813f2ed7667
Ignacio <ignacio@liveclicker.com>
2019-01-03 12:36:02 -0300

    Look for model associations and load them on instance build

--- packages/sequelize-transparent-cache/lib/cache/util.js
+++ packages/sequelize-transparent-cache/lib/cache/util.js
@@ -1,13 +1,25 @@
 function instanceToData (instance) {
-  return instance.get({plain: true})
+  return instance.get({ plain: true })
 }
 
 function dataToInstance (model, data) {
   if (!data) {
     return data
   }
-
-  const instance = model.build(data, { isNewRecord: false })
+  let include = []
+  if (model.associations) {
+    Object.keys(model.associations).forEach((key) => {
+      const association = {}
+      //  model.associations[key] does not work on include, we grab it from sequelize.model()
+      if (model.associations[key].hasOwnProperty('options')) {
+        let modelName = model.associations[key].options.name.singular
+        association.model = model.sequelize.model(modelName)
+        association.as = key
+      }
+      include.push(association)
+    })
+  }
+  const instance = model.build(data, { isNewRecord: false, include })
 
   if (data.updatedAt) {
     instance.setDataValue('updatedAt', data.updatedAt)
