4cece4c85027ebaa40bb2ba11ab3c4b7fc128589
Steve Foster <steve.foster@madecurious.com>
2026-05-29 08:59:42 +1200

    chore(ci): improve linting config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 import pluginPromise from 'eslint-plugin-promise'
 import pluginNode from 'eslint-plugin-n'
+import pluginJest from 'eslint-plugin-jest'
+import pluginImport from 'eslint-plugin-import'
 
 export default defineConfig([
   {
-    files: ['**/*.js'],
-  },
-  {
-    ignores: ['coverage/*'],
+    ignores: ['coverage/*', 'examples/*'],
   },
   eslintConfigPrettier,
   pluginPromise.configs['flat/recommended'],
   pluginNode.configs['flat/recommended'],
+  pluginJest.configs['flat/recommended'],
+  pluginImport.flatConfigs.recommended,
 ])
